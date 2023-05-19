3886a3ac87152627af2ca620ca14d47af7f2f484
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2023-05-19 11:30:59 +0200

    Add code written in class

--- prolog/core.pl
+++ prolog/core.pl
@@ -11,3 +11,45 @@ color(blau):-write("\e[1;94m").
 color(lila):-write("\e[1;95m").
 color(cel):-write("\e[1;96m").
 color(blanc):-write("\e[1;97m").
+
+% T, [T] -> [T]
+esborrar(_, [], []).
+esborrar(X, [X|L], L).
+esborrar(X, [Y|L1], [Y|L2]) :-
+    esborrar(X, L1, L2).
+
+% T, [T] -> int
+vegades(_, [], 0).
+vegades(X, [X|L], N) :- 
+    vegades(X, L, NL),
+    N is NL + 1.
+vegades(X, [_|L], N) :-
+    vegades(X, L, N).
+
+% T, [T] -> int
+seguits(_, [], 0).
+seguits(X, [X|L], N) :-
+    seguits(X, L, NL),
+    N is NL + 1.
+seguits(_, [_|_], 0).
+
+% TODO: Add signatures
+
+treupistes([],[]).
+treupistes([X|L1],[Y|L2]):-
+    convertir(X,Y),
+    treupistes(L1,L2).
+
+convertir([],[]).
+convertir([X|L1],[[seguits, X, 1]|L2]) :-
+    vegades(X,[X|L1],1),
+    convertir(L1,L2).
+convertir([X|L1],[[seguits, X, N]|L2]) :-
+    vegades(X,[X|_],N),
+    seguits(X,[L1],N),
+    esborrar(X,[X|L1],L3),
+    convertir(L3,L2).
+convertir([X|L1],[[no_seguits, X, N]|L2]) :-
+    vegades(X,[X|_],N), 
+    esborrar(X,[X|L1],L3),
+    convertir(L3,L2).
